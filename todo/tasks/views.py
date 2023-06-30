5d0b37a4932c2a1ce64165831df18715a8220921
Sufian <98504017+SufianB76@users.noreply.github.com>
2023-06-30 00:27:53 -0400

    Update views.py

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -19,7 +19,7 @@ def index(request):
         return redirect('/')
 
     context = {'tasks': tasks, 'form': form}
-    return render(request, 'tasks/list.html', context)
+    return render(request, 'tasks/index.html', context)
 
 
 def updateTask(request, pk):
